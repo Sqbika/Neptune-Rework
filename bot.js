7dc3f52086d3f54d69274db67d73977a2879502d
Mr.Sqbika <Mr.Sqbika>
2016-08-14 23:20:23 +0200

    Initial Commit

---
+++ bot.js
@@ -0,0 +1,26 @@
+var INITIALIZATION_MS = new Date();
+var discord = require('discordj.js');
+var helper = require('./helper.js');
+
+var DEV = false;
+
+process.argv.forEach(function (val, index, array) { 
+    if (val === "dev") DEV = true;
+});
+
+if (DEV) {
+    var auth = require('./login/dev_bot.json');
+} else {
+    var auth = require('./login/bot.json');
+}
+
+var bot = new Discord.Client();
+
+
+/* Initializaton phase */
+
+bot.loginWithToken(auth.key);
+
+bot.on("ready", function () {
+    
+});
